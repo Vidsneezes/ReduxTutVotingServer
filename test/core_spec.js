f8b001fcf5654ffbadfb48e5cf63a2e73196df30
Vidsneezes <moraleszez@gmail.com>
2017-03-17 10:12:15 -0600

    added core.js, made test for core js

---
+++ test/core_spec.js
@@ -0,0 +1,21 @@
+import {List,Map} from 'immutable';
+import {expect} from 'chai';
+
+import {setEntries} from '../src/core.js';
+
+describe('application logic',()=>{
+
+    describe('setEntries', ()=>{
+
+        it('adds the entries to the state', () => {
+            const state = Map();
+            const entries = List.of('Transpotting', '28 Days Later');
+            const nextState = setEntries(state, entries);
+
+            expect(nextState).to.equal(Map({
+                entries: List.of('Transpotting', '28 Days Later')
+            }));
+        })
+
+    });
+});
