c008e184863c1e8e71d8d8b201ebd0488805e9b6
Vidsneezes <moraleszez@gmail.com>
2017-03-17 10:16:21 -0600

    implemented test for converting to immutable regualr arrays

--- test/core_spec.js
+++ test/core_spec.js
@@ -9,11 +9,20 @@ describe('application logic',()=>{
 
         it('adds the entries to the state', () => {
             const state = Map();
-            const entries = List.of('Transpotting', '28 Days Later');
+            const entries = List.of('Trainspotting', '28 Days Later');
             const nextState = setEntries(state, entries);
 
             expect(nextState).to.equal(Map({
-                entries: List.of('Transpotting', '28 Days Later')
+                entries: List.of('Trainspotting', '28 Days Later')
+            }));
+        })
+
+        it('converts to immutable', () => {
+            const state = Map();
+            const entries = ['Trainspotting','28 Days Later'];
+            const nextState = setEntries(state,entries);
+            expect(nextState).to.equal(Map({
+                entries: List.of('Trainspotting','28 Days Later')
             }));
         })
 
