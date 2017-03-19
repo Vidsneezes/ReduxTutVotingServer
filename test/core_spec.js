ce24a61922616d38edaf4a2d48030bf0bceab411
Vidsneezes <moraleszez@gmail.com>
2017-03-19 16:07:32 -0600

    added test for tied entries in tally

--- test/core_spec.js
+++ test/core_spec.js
@@ -61,6 +61,26 @@ describe('application logic',()=>{
                 entries:List.of('127 Hours','Trainspotting')
             }));
         });
+
+        it('puts both from tied back to entries', ()=>{
+            const state = Map({
+                vote: Map({
+                    pair:List.of('Trainspotting','28 Days Later'),
+                    tally: Map({
+                        'Trainspotting':3,
+                        '28 Days Later':3
+                    })
+                }),
+                entries: List.of('Sunshine','Millions','127 Hours')
+            });
+            const nextState = next(state);
+            expect(nextState).to.equal(Map({
+                vote: Map({
+                    pair:List.of('Sunshine','Millions')
+                }),
+                entries: List.of('127 Hours','Trainspotting','28 Days Later')
+            }));
+        })
     });
 
     describe('vote',() =>{
